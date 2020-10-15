4585734b6c7f6869497e488963a278a2b111e031
marthadelrio <57366798+marthadelrio@users.noreply.github.com>
2020-10-15 13:49:09 -0500

    Update ComputerConnections.cpp

--- src/ComputerConnections.cpp
+++ src/ComputerConnections.cpp
@@ -15,8 +15,10 @@ ComputerConnections::ComputerConnections(Register &r) {
     cout << "Logging connections... " << setw(26);
     // fill up stack and queue
     for(int i = 0; i < r.get_size(); i++) {
+        if(r.get_entry(i).get_ip_dest() == ip) {
+            incoming.push(r.get_entry(i).get_ip_source());
+        }
         if(r.get_entry(i).get_ip_source() == ip) {
-            incoming.push(r.get_entry(i).get_ip_dest());
             outgoing.push(r.get_entry(i).get_ip_dest());
         }
     }
@@ -28,7 +30,7 @@ ComputerConnections::~ComputerConnections() {};
 
 void ComputerConnections::generate_ip(string home) {
     // make random number from 1 to 150
-    srand(time(0));
+    srand(int(time(0)));
     int last = rand()%150 + 1;
 
     // save new ip address in attribute ip
@@ -36,9 +38,9 @@ void ComputerConnections::generate_ip(string home) {
     ip = home;
 }
 
-int ComputerConnections::get_incoming_size() { return incoming.size(); }
+int ComputerConnections::get_incoming_size() { return int(incoming.size()); }
 
-int ComputerConnections::get_outgoing_size() { return outgoing.size(); }
+int ComputerConnections::get_outgoing_size() { return int(outgoing.size()); }
 
 string ComputerConnections::get_last_ip() {
     return incoming.top();
@@ -56,10 +58,17 @@ string ComputerConnections::check_dest(string dest_ip) {
 }
 
 void ComputerConnections::print_all() {
-    int size = incoming.size();
+    int size = int(incoming.size());
     for(int i = 0; i < size; i++) {
-        cout << incoming.top() << "\t" << outgoing.front() << endl;
-        incoming.pop();
-        outgoing.pop();
+        cout << incoming.top() << endl;
     }
+    size = int(outgoing.size());
+    cout << endl;
+    for(int i = 0; i < size; i++) {
+        cout << outgoing.front() << endl;
+    }
+}
+
+bool ComputerConnections::check_consecutive(int n) {
+    while()
 }
